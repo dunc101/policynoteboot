c7d2f07e13644b7911aa47ad2c2e272d74383b92
dustin.duncan <dustin.duncan@ad.ipacc.com>
2018-01-22 15:50:57 -0600

    trying to fix release artifact

--- scripts/ci/upload-nexus-artifact.sh
+++ scripts/ci/upload-nexus-artifact.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 VERSION=$1
-FILE_LOCATION=$2
-NEXUS_REPO=$3
+NEXUS_REPO=$2
 
 echo "About to upload artifact to Nexus"
 mvn deploy-deploy-file deploy:deploy-file -DgroupId=${project.groupId} \
