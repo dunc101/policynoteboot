2e702f857ed7a94cebe5084109ef9cad731a79db
dustin.duncan <dustin.duncan@ad.ipacc.com>
2018-01-22 16:30:22 -0600

    Cleaning up scripts

--- scripts/ci/upload-nexus-artifact.sh
+++ scripts/ci/upload-nexus-artifact.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 POM_DIRECTORY=$1
 VERSION=$2
 PACKAGE_TYPE=$3
 
-# Let's copy the settings.xml file to the correct location so that we have acces to the Infinity jar files
+# Let's copy the settings.xml file to the correct location so that we can look up the artifactId and groupId's with maven.
 mkdir ${HOME}/.m2
 cp resource-main-code/settings.xml ${HOME}/.m2/
 
@@ -16,8 +17,7 @@ echo "Getting the GROUP_ID"
 GROUP_ID=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.groupId}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null`
 # Go back to the start directory.... 
 cd $MAIN_DIRECTORY
-NEW_DIR=`pwd`
-echo "Curernt new dir is ${NEW_DIR}"
+
 # Move the jar to a generic name so we can easily upload it
 mv artifact/*.jar artifact/app.jar
 
@@ -34,8 +34,3 @@ curl -v \
 -F hasPom=false \
 -F "file=@./artifact/app.jar" \
 http://maven01.ipacc.com:8081/nexus/service/local/artifact/maven/content
-#mvn deploy:deploy-file -DgroupId=${GROUP_ID} \
-#  -DartifactId=${ARTIFACT_ID} \
-#  -Dversion=${VERSION} \
-#  -Dpackaging=jar \
-#  -Durl=${NEXUS_REPO}
