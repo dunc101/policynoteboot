661dfbed06503918dbaed312fee25200dd8cb89a
dustin.duncan <dustin.duncan@ad.ipacc.com>
2018-01-22 15:55:48 -0600

    release artifact

--- scripts/ci/upload-nexus-artifact.sh
+++ scripts/ci/upload-nexus-artifact.sh
@@ -2,8 +2,11 @@
 VERSION=$1
 NEXUS_REPO=$2
 
-echo "About to upload artifact to Nexus"
-mvn deploy-deploy-file deploy:deploy-file -DgroupId=${project.groupId} \
+ARTIFACT_ID=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null`
+GROUP_ID=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.groupId}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null`
+
+echo "About to upload artifact to Nexus ${ARTIFACT_ID}:${GROUP_ID}:${VERSION}"
+mvn deploy:deploy-file -DgroupId=${project.groupId} \
   -DartifactId=${project.artifactId} \
   -Dversion=${VERSION} \
   -Dpackaging=${project.packaging} \
